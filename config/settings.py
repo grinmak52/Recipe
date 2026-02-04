d91e374269f5015aa2857803195a0dc12f8f7ff7
Skrip01 <168160103+Skrip01@users.noreply.github.com>
2026-02-05 00:37:02 +0500

    Add user authentication(login,logout,register)

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import LOGOUT_REDIRECT_URL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,3 +123,7 @@ STATIC_URL = "static/"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = 'index'
--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('recipes.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 if settings.DEBUG:
